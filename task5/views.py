1b7a2cd907fd8a33fef0bcb9b2c037ec62bfecc4
module_18_2 <erjan03iopro@gmail.com>
2024-11-14 16:32:33 +0600

    module_18_5

--- task5/views.py
+++ task5/views.py
@@ -6,22 +6,34 @@ users = ['Erzhan', 'Volodya', 'Davis', 'Ashot']
 
 
 def sign_up_by_django(request):
+    info = {}
     if request.method == 'POST':
         form = UserRegister(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            repeat_password = form.cleaned_data['repeat_password']
-            age = form.cleaned_data['age']
-
-            print(name)
-            print(password)
-            print(repeat_password)
-            print(age)
-            return HttpResponse('Форма успешно отправлено!')
+            info['name'] = form.cleaned_data['username']
+            info['password'] = form.cleaned_data['password']
+            info['repeat_password'] = form.cleaned_data['repeat_password']
+            info['age'] = form.cleaned_data['age']
+            if info['password'] != info['repeat_password']:
+                info['error'] = 'Пароли не совпадают!'
+                return render(request, 'fifth_task/registration_page.html', {'form': form, 'info': info})
+            elif info['name'] in users:
+                info['error'] = 'Такой пользователь существует!'
+                return render(request, 'fifth_task/registration_page.html', {'form': form, 'info': info})
+            elif int(info['age']) < 18:
+                info['error'] = 'Вы должны быть старше 18!'
+                return render(request, 'fifth_task/registration_page.html', {'form': form, 'info': info})
+            else:
+                info['error'] = 'Success'
+                return render(request, 'fifth_task/registration_page.html', {'form': form, 'info': info, 'username': info['name']})
+            # print(name)
+            # print(password)
+            # print(repeat_password)
+            # print(age)
     else:
         form = UserRegister()
+        info['error'] = 'Empty'
         print('Failed==============================>')
-    return render(request, 'fifth_task/registration_page.html', {'form': form})
+    return render(request, 'fifth_task/registration_page.html', {'form': form, 'info': info})
 
 
